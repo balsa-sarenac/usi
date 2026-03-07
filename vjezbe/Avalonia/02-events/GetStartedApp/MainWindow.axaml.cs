e24c3b2483797b15c15f267757db5997494e42ff
Marija Djurdjevic <masadjurdjevic02@gmail.com>
2026-03-07 20:17:31 +0100

    Add 01-xaml and 02-events examples and screenshots

---
+++ vjezbe/Avalonia/02-events/GetStartedApp/MainWindow.axaml.cs
@@ -0,0 +1,30 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace GetStartedApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        var button = this.FindControl<Button>("CodeBehindButton");
+        if (button is not null)
+        {
+            button.Click += CodeBehindButton_Click;
+        }
+
+    }
+    
+    private void XamlButton_Click(object? sender, RoutedEventArgs e)
+    {
+        Title = "Kliknuto preko XAML-a!";
+    }
+
+    private void CodeBehindButton_Click(object? sender, RoutedEventArgs e)
+    {
+        Title = "Kliknuto preko code-behind-a!";
+    }
+}
+
